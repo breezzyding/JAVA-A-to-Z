78fe197d7a67aeec991b7f32d1ebc30afddb3f59
easymoneysniper <kevinding35@163.com>
2021-08-23 17:50:54 +0800

    8-23 √
    今天人是晕的，明天再来。

--- chapter08/src/com/dinglearn/Object_/Finalize_.java
+++ chapter08/src/com/dinglearn/Object_/Finalize_.java
@@ -4,6 +4,26 @@ public class Finalize_ {
     public static void main(String[] args) {
 
         Car bmw = new Car("宝马");
+
+        //此时car对象就是一个垃圾,垃圾回收器就会回收(销毁)对象
+        //在销毁对象前,会调用该对象的finalize方法
+        //程序员就可以在finalize中,写自己的业务逻辑代码
+        //如释放资源,数据库连接,或者打开文件...
+        //如程序员不重写finalize方法,就会调用Object类的finalize,即默认处理
+        bmw = null;
+
+        System.gc();//主动调用垃圾回收器
+
+        System.out.println("程序退出了...");
+        /*
+            输出:
+            程序退出了...
+            销毁汽车宝马
+            释放了某些资源...
+
+            System.gc();不会阻塞程序的进行
+            因此先执行System.out.println("程序退出了...");
+         */
     }
 }
 
@@ -12,5 +32,13 @@ class Car {
 
     public Car(String name) {
         this.name = name;
+    }
+    //重写finalize方法
+
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println("销毁汽车->" + name);
+        System.out.println("释放了某些资源...");
     }
 }
